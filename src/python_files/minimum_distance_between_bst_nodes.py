7adf3c157ddbd3b0948432fa541251c73eefff02
kim <kim>
2023-06-17 22:39:10 -0400

    first automation

---
+++ src/python_files/minimum_distance_between_bst_nodes.py
@@ -0,0 +1,17 @@
+
+"""
+LeetCode Problem: Minimum Distance Between BST Nodes
+LeetCode Link: https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+
+Problem Description:
+[Provide a brief description of the problem]
+
+Solution:
+"""
+
+def your_function_name(input_parameters):
+    # Your solution code goes here
+    pass
+
+# Test cases
+# Add test cases to validate your solution
